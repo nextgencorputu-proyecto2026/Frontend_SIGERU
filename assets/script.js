fe807986e288e9e35f7f3a107cba992ce5568fe7
Leo Barreneche <leobarreneche28@gmail.com>
2026-07-22 17:44:34 -0300

    Create view home, ListaCamiones and function for table Camiones

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,57 @@
 
+/* ---------------------
+    CAMIONES
+--------------------- */
 
+// Funcion para cargar la tabla con los datos de los camiones
+function cargarTabla(camiones) {
+
+    const tabla = document.getElementById("tablaCamiones");
+
+    tabla.innerHTML = "";
+
+    camiones.forEach(camion => {
+
+        tabla.innerHTML += `
+            <tr>
+
+                <th scope="row">${camion.id}</th>
+
+                <td>${camion.matricula}</td>
+
+                <td>${camion.tipo}</td>
+
+                <td>${camion.ruta}</td>
+
+                <td>${camion.estado}</td>
+
+                <td>
+
+                    <div class="list-group-horizontal">
+
+                        <button class="verDatos btn btn-outline-primary btn-sm"
+                            data-bs-toggle="modal" data-bs-target="#datosCamiones">
+                            Ver
+                        </button>
+
+                        <button class="btn btn-outline-secondary btn-sm">
+                            Editar
+                        </button>
+
+                        <button class="btn btn-outline-danger btn-sm">
+                            Eliminar
+                        </button>
+
+                    </div>
+
+                </td>
+
+            </tr>
+        `;
+
+    });
+
+}
 
 
 
